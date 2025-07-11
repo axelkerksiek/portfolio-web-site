c376a4f8dbde7caf9be7f7da1fa6e22ea960a0f1
Axel Kerksiek <axelkerksiek@gmail.com>
2025-07-10 21:51:10 -0600

    added icons

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// Your app logic here
 </script>
 
 <template>
-  <div class="flex justify-center space-x-8">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>
+      <i class="fa-solid fa-hands"></i>      Welcome to Your Portfolio
+    </h1>
+    <p>Start building your amazing portfolio here!</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  text-align: center;
+  padding: 2rem;
 }
 </style>
