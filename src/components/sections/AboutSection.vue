ab7745327bc5ddd3536318b61c3624eab718d0b5
axelkerksiek <axelkerksiek@gmail.com>
2025-11-23 21:23:52 -0700

    fix: Updated about me section

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -18,7 +18,7 @@
         <p class="text-text-muted text-lg leading-loose">
           I'm a full-stack developer specializing in
           <span class="font-semibold italic">Vue</span> and
-          <span class="font-semibold italic">Rails</span>. I work with the entire product
+          <span class="font-semibold italic">Ruby on Rails</span>. I work with the entire product
           lifecycle—from design and engineering through testing and deployment. Below are a few
           personal projects that I have worked on. Feel free to explore the live sites or dive into
           my code on GitHub.
